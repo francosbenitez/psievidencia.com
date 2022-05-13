67935dba9da9ff975350fe28de96ecf3752f05b0
francobenitez <franco.benitez@hogarthww.com>
2022-05-13 10:52:15 -0300

    feat: add tailwind to the project

--- src/components/PsychologistsCards.js
+++ src/components/PsychologistsCards.js
@@ -2,22 +2,24 @@ import React from "react";
 
 const PsychologistsCards = ({ psychologists }) => {
   return (
-    <div>
+    <ul className="divide-y divide-gray-200">
       {psychologists.map((item) => {
         return (
-          <div key={item.Name}>
-            <h1>{item["Name"]}</h1>
-            <p>{item["Theorical discipline"]}</p>
-            <p>{item["Specialization"]}</p>
+          <li key={item.Name} className="py-4 flex">
             <img
               className="h-10 w-10 rounded-full"
               src={item["Image URL"]}
               alt=""
             />
-          </div>
+            <div className="ml-3">
+              <p>{item["Name"]}</p>
+              <p>{item["Theorical discipline"]}</p>
+              <p>{item["Specialization"]}</p>
+            </div>
+          </li>
         );
       })}
-    </div>
+    </ul>
   );
 };
 
