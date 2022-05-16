217de5964f3ef77ce680ddc77f42d5fa847bb5e0
francobenitez <franco.benitez@hogarthww.com>
2022-05-16 14:36:11 -0300

    feat: print each psychologist data in the detailed page

--- src/views/psychologists-detail.js
+++ src/views/psychologists-detail.js
@@ -2,23 +2,17 @@ import React from "react";
 
 const PsychologistsDetail = ({ psychologist }) => {
   return (
-    <div class="container mx-auto py-28">
+    <div className="container mx-auto py-28">
       <h1 className="text-center font-bold text-5xl mb-5">
         {psychologist.name}
       </h1>
       <ul>
-        <li>
-          <span className="font-bold underline">Gender Identity</span>:{" "}
-          {psychologist.gender}
-        </li>
-        <li>
-          <span className="font-bold underline">Registration Type</span>:{" "}
-          {psychologist.registration_type}
-        </li>
-        <li>
-          <span className="font-bold underline">Additional Data</span>:{" "}
-          {psychologist.additional_data}
-        </li>
+        {Object.keys(psychologist).map((item, index) => (
+          <li key={index}>
+            <span className="font-bold underline">{item}</span>:{" "}
+            {psychologist[item]}
+          </li>
+        ))}
       </ul>
     </div>
   );
