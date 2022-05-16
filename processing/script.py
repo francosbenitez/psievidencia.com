2269a68718e66e74e0594adc264555a5b9e148d9
francobenitez <franco.benitez@hogarthww.com>
2022-05-16 17:55:39 -0300

    feat: format education data

--- processing/script.py
+++ processing/script.py
@@ -5,9 +5,16 @@ import pandas as pd
 r = requests.get('https://docs.google.com/spreadsheets/d/e/2PACX-1vQngt5TxTabbOavo5qHaZz5ohs9o_46sWrhQMKT5gJdedIG3Icq0qvuUX1dfdkcrmqNUxzCjOk2egSo/pub?gid=160193944&single=true&output=csv')
 data = r.content
 
-df = pd.read_csv(BytesIO(data), index_col=0,parse_dates=['Marca temporal'])
+df = pd.read_csv(BytesIO(data), index_col=0, parse_dates=['Marca temporal'])
 
-# column_names = {"name": "newName"}
-# df.rename(columns = column_names, inplace=True)
+column_names = {"Mayor grado académico alcanzado": "education"}
+df.rename(columns = column_names, inplace=True)
 
-print(df)
+df_education = df["education"].value_counts().to_frame().reset_index()
+df_education.rename(columns = {"index": "Education", "education": "Value"}, inplace=True)
+
+print(df_education)
+
+df_education.to_csv('./public/data/education.csv', index=False) 
+
+
