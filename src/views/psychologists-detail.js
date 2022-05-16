abcccf3708b4312a6448e4af7cb7da7b37b07cf1
francobenitez <franco.benitez@hogarthww.com>
2022-05-16 13:17:27 -0300

    feat: add more data to detailed page

--- src/views/psychologists-detail.js
+++ src/views/psychologists-detail.js
@@ -3,11 +3,23 @@ import React from "react";
 const PsychologistsDetail = ({ psychologist }) => {
   return (
     <div class="container mx-auto py-28">
-      Psychologist ID: {psychologist.id}
-      <br />
-      Psychologist name: {psychologist.name}
-      <br />
-      Additional data: {psychologist.additional_data}
+      <h1 className="text-center font-bold text-5xl mb-5">
+        {psychologist.name}
+      </h1>
+      <ul>
+        <li>
+          <span className="font-bold underline">Gender Identity</span>:{" "}
+          {psychologist.gender}
+        </li>
+        <li>
+          <span className="font-bold underline">Registration Type</span>:{" "}
+          {psychologist.registration_type}
+        </li>
+        <li>
+          <span className="font-bold underline">Additional Data</span>:{" "}
+          {psychologist.additional_data}
+        </li>
+      </ul>
     </div>
   );
 };
