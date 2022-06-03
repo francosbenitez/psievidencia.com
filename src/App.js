5ef96f3de8097e3fc70ad5b0c7e50544e73a90e8
francobenitez <franco.benitez@hogarthww.com>
2022-06-03 00:59:55 -0300

    refactor: remove papaparse & implement paginated results

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import Papa from "papaparse";
 import { useState, useEffect } from "react";
 import "./assets/css/default.css";
 import { Routes, Route } from "react-router-dom";
@@ -9,75 +8,17 @@ import ScrollToTop from "./components/ScrollToTop";
 import PsychologistsService from "./services/PsychologistsService";
 
 function App() {
-  const psychologistsUrl =
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vQngt5TxTabbOavo5qHaZz5ohs9o_46sWrhQMKT5gJdedIG3Icq0qvuUX1dfdkcrmqNUxzCjOk2egSo/pub?gid=160193944&single=true&output=csv";
-
   const [psychologists, setPsychologists] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  async function fetchData() {
-    const data = (await PsychologistsService.index(1)).data;
-    console.log("data from fetchData", data);
-  }
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const fetchPsychologists = () => {
+  const fetchPsychologists = async () => {
     setLoading(true);
-    Papa.parse(psychologistsUrl, {
-      download: true,
-      beforeFirstChunk: function (chunk) {
-        var index = chunk.match(/\r\n|\r|\n/).index;
-        console.log("index", index);
-        var headings = chunk
-          .substr(0, index)
-          .split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
+    const data = (await PsychologistsService.index(1)).data;
 
-        headings[0] = "date";
-        headings[1] = "name";
-        headings[2] = "email";
-        headings[3] = "gender";
-        headings[4] = "registration_type";
-        headings[5] = "registration_number";
-        headings[6] = "institution";
-        headings[7] = "team";
-        headings[8] = "province";
-        headings[9] = "city";
-        headings[10] = "education";
-        headings[11] = "therapeutic_model";
-        headings[12] = "gender_perspective";
-        headings[13] = "specialization";
-        headings[14] = "work_population";
-        headings[15] = "work_modality";
-        headings[16] = "online";
-        headings[17] = "prepaid";
-        headings[18] = "prepaid_type";
-        headings[19] = "invoice";
-        headings[20] = "sign_language";
-        headings[21] = "session_languages";
-        headings[22] = "social_networks";
-        headings[23] = "phone_number";
-        headings[24] = "additional_data";
-        headings[25] = "name_2";
-        return headings.join() + chunk.substr(index);
-      },
-      header: true,
+    console.log("data from fetchPsychologists", data);
 
-      complete: function (results) {
-        results.data.forEach((psychologist, i) => {
-          psychologist.id = i + 1;
-        });
-
-        results.data.sort(() => Math.random() - 0.5);
-
-        console.log(results.data);
-
-        setPsychologists(results.data);
-        setLoading(false);
-      },
-    });
+    setPsychologists(data.results);
+    setLoading(false);
   };
 
   useEffect(() => {
--- src/views/home.js
+++ src/views/home.js
@@ -23,6 +23,11 @@ const Home = ({ psychologists, loading }) => {
           A web app to help you find the best evidence-based psychologists
         </h2>
         <Filter psychologists={psychologists} loading={loading} />
+        <div class="flex justify-center">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Load more
+          </button>
+        </div>
       </div>
       <footer className="bottom-0 fixed bg-white w-full p-8 text-center">
         To be part of this collection, fill your data in{" "}
