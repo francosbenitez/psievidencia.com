4d0498f0365dd7d7318735e01dddbe9d00395461
francobenitez <franco.benitez@hogarthww.com>
2022-07-06 22:04:51 -0300

    feat: add icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <title>PsiEvidencia</title>
+      </Head>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
 };
 
 export default Home;
