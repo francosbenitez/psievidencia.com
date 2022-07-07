0e86a9f2a97800505bc9f0f1ec4562ae21aa797a
francobenitez <franco.benitez@hogarthww.com>
2022-07-07 01:00:47 -0300

    fix: icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "custom-image": "url('/icons/arrow-left.svg')",
+      },
+    },
   },
   plugins: [],
 };
