f2d8d3c6216b6509a7fdd371b3877575ca7b408e
francobenitez <franco.benitez@hogarthww.com>
2022-07-06 22:15:45 -0300

    feat: add components as .tsx files

---
+++ components/home/SearchName.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SearchName = ({ handleNameChange }) => {
+  return (
+    <input
+      className="border-solid h-10 border-2 border-indigo-600 w-full pl-3 mb-6"
+      placeholder="Search by name"
+      onChange={handleNameChange}
+    />
+  );
+};
+
+export default SearchName;
---
+++ components/home/TheFooter.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { ReactComponent as GitHub } from "../../assets/icons/github.svg";
+
+const HomeFooter = () => {
+  return (
+    <footer className="bottom-0 fixed bg-white w-full p-8 text-center">
+      To be part of this collection, fill your data in{" "}
+      <a
+        className="underline"
+        target="_blank"
+        rel="noreferrer"
+        href="https://docs.google.com/forms/d/e/1FAIpQLSccyO5jICweFShGTLEEiCOYLYySlEUacI0_4IDCY10AdYqIpA/viewform"
+      >
+        this Google Forms
+      </a>{" "}
+      |{" "}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://github.com/francosbenitez/psievidencia"
+      >
+        <span className="underline">Source code</span>{" "}
+        <GitHub className="inline w-4 h-4" />
+      </a>
+    </footer>
+  );
+};
+
+export default HomeFooter;
