2fbcef9369d2a51c3008bebe5367a03b7fc35a72
francobenitez <franco.benitez@hogarthww.com>
2022-07-11 15:31:35 -0300

    feat: allow fetching fonts from the server

--- components/home/TheCard.tsx
+++ components/home/TheCard.tsx
@@ -6,64 +6,62 @@ const TheCard = ({ psychologist }: { psychologist: Psychologist }) => {
   return (
     <Link href={`/psychologists/${psychologist.id}`}>
       <div className="w-full rounded shadow-md bg-white p-10 flex flex-col border-2 hover:border-primary cursor-pointer">
-        <header className=" text-2xl font-extrabold py-4 px-4 text-center">
+        <header className=" text-xl font-extrabold mb-3 text-center">
           {psychologist.name !== "" ? psychologist.name : "Unknown"}
         </header>
-        <div>
-          <ul className="text-primary text-center font-semibold break-words">
-            <li>{psychologist.province}</li>
-            <li className="text-left">
-              {psychologist.therapeutic_model.split(",").map(
-                (item, index) =>
-                  index < 3 && (
-                    <span
-                      className="bg-tertiary text-white break-all mr-1 px-1"
-                      key={item}
-                    >
-                      {item}
-                    </span>
-                  )
-              )}
-              {psychologist.therapeutic_model.split(",").length > 3 && (
-                <span>...</span>
-              )}
-            </li>
-            <li className="text-left">
-              {psychologist.work_population.split(",").map(
-                (item, index) =>
-                  index < 3 && (
-                    <span
-                      className="bg-fourth text-white break-all mr-1 px-1"
-                      key={item}
-                    >
-                      {item}
-                    </span>
-                  )
-              )}
-              {psychologist.work_population.split(",").length > 3 && (
-                <span>...</span>
-              )}
-            </li>
-            {psychologist.specialization !== "" && (
-              <li className="text-left">
-                {psychologist.specialization.split(",").map(
-                  (item, index) =>
-                    index < 3 && (
-                      <span
-                        className="bg-fifth text-white break-all mr-1 px-1"
-                        key={item}
-                      >
-                        {item}
-                      </span>
-                    )
-                )}
-                {psychologist.specialization.split(",").length > 3 && (
-                  <span>...</span>
-                )}
-              </li>
+        <ul className="text-primary text-center font-semibold break-words">
+          <li className="mb-3">{psychologist.province}</li>
+          <li className="text-left">
+            {psychologist.therapeutic_model.split(",").map(
+              (item, index) =>
+                index < 3 && (
+                  <span
+                    className="bg-tertiary text-white break-all mr-1 px-1"
+                    key={item}
+                  >
+                    {item}
+                  </span>
+                )
             )}
-          </ul>
-        </div>
+            {psychologist.therapeutic_model.split(",").length > 3 && (
+              <span>...</span>
+            )}
+          </li>
+          <li className="text-left">
+            {psychologist.work_population.split(",").map(
+              (item, index) =>
+                index < 3 && (
+                  <span
+                    className="bg-fourth text-white break-all mr-1 px-1"
+                    key={item}
+                  >
+                    {item}
+                  </span>
+                )
+            )}
+            {psychologist.work_population.split(",").length > 3 && (
+              <span>...</span>
+            )}
+          </li>
+          {psychologist.specialization !== "" && (
+            <li className="text-left">
+              {psychologist.specialization.split(",").map(
+                (item, index) =>
+                  index < 3 && (
+                    <span
+                      className="bg-fifth text-white break-all mr-1 px-1"
+                      key={item}
+                    >
+                      {item}
+                    </span>
+                  )
+              )}
+              {psychologist.specialization.split(",").length > 3 && (
+                <span>...</span>
+              )}
+            </li>
+          )}
+        </ul>
         <footer className="text-center py-3 text-primary flex justify-center mt-auto block">
           {psychologist.online === "Sí" && (
             <p className="p-2">
