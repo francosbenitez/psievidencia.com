07697ff54eac84e3492e350899825e8db1218581
francobenitez <franco.benitez@hogarthww.com>
2022-07-13 19:35:45 -0300

    style: change theheader in mb

--- components/home/TheHeader.tsx
+++ components/home/TheHeader.tsx
@@ -6,9 +6,9 @@ const TheHeader = () => {
   return (
     <>
       <h1 className="text-center text-5xl">
-        <Magnifier className="inline w-12 h-12" />
+        <Magnifier className="w-12 h-12 hidden sm:inline" />
         <span className="sm:px-4 text-primary">Psievidencia</span>
-        <Magnifier className="inline w-12 h-12" />
+        <Magnifier className="w-12 h-12 hidden sm:inline" />
       </h1>
       <h2 className="text-center text-2xl my-9 text-primary">
         <FormattedMessage id="title" />
