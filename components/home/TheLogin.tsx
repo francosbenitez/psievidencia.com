a03432e713231b33d11f2d7575714b915331c9fa
francobenitez <franco.benitez@hogarthww.com>
2022-07-20 00:34:11 -0300

    feat: add thelogin component

---
+++ components/home/TheLogin.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import UsersService from "../../services/UsersService";
+
+const TheLogin = () => {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    try {
+      const form = {
+        username: username,
+        password: password,
+      };
+
+      let formData = new FormData();
+      Object.entries(form).forEach(([key, value]) => {
+        formData.append(key, value);
+      });
+
+      (await UsersService.login(formData)).data.data;
+    } catch (errors) {
+      console.log("errors.response.data", errors.response.data);
+    }
+  };
+
+  return (
+    <>
+      <p className="pb-4">¿No estás registrado? Registrate</p>
+      <form className="w-full" onSubmit={handleSubmit}>
+        <label>
+          Usuario
+          <input
+            type="text"
+            className="h-10 sm:h-full border border-primary w-full pl-3 outline-0 rounded dropdown-header"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </label>
+        <label>
+          Contraseña
+          <input
+            type="text"
+            className="h-10 sm:h-full border border-primary w-full pl-3 outline-0 rounded dropdown-header"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </label>
+        <button
+          type="submit"
+          className="bg-primary text-white py-2 px-4 rounded"
+        >
+          Loguearse
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default TheLogin;
