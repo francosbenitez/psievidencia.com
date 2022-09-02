79ecae0f4e6c30b6e39d6a580038d108225dd66a
francobenitez <franco.benitez@hogarthww.com>
2022-09-02 12:59:43 -0300

    refactor: remove usescrollblock from themodal

--- components/global/TheModal.tsx
+++ components/global/TheModal.tsx
@@ -4,7 +4,7 @@ import React, {
   useRef,
   useImperativeHandle,
 } from "react";
-import { useScrollBlock } from "@/hooks/useScrollBlock";
+// import { useScrollBlock } from "@/hooks/useScrollBlock";
 
 type Props = {
   button: React.ReactElement<any, string | React.JSXElementConstructor<any>>;
@@ -31,10 +31,10 @@ const TheModal = (props: any, ref: any) => {
     count,
   }: Props = props;
 
-  const [blockScroll, allowScroll] = useScrollBlock();
+  // const [blockScroll, allowScroll] = useScrollBlock();
 
   const showModal = () => {
-    !show ? blockScroll() : allowScroll();
+    // !show ? blockScroll() : allowScroll();
     setShow(!show);
   };
 
