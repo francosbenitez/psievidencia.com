0b50b4684531d09f7097ce23a17851c310abbeb5
francobenitez <franco.benitez@hogarthww.com>
2022-09-12 17:15:06 -0300

    feat: add account & profile icons

--- pages/edit.tsx
+++ pages/edit.tsx
@@ -3,6 +3,8 @@ import Head from "next/head";
 import * as EditProfileComponents from "@/components/edit/profile";
 import * as EditAccountComponents from "@/components/edit/account";
 import SwitchComponents from "@/components/edit/SwitchComponents";
+import Account from "@/public/icons/account.svg";
+import Profile from "@/public/icons/profile.svg";
 
 const Edit = () => {
   const ProfileComponents: Record<string, any> = EditProfileComponents;
@@ -18,12 +20,22 @@ const Edit = () => {
       <div className="container min-h-screen w-11/12 mx-auto pt-20 pb-40">
         <h2 className="text-3xl">Editá tus datos</h2>
         <div className="flex">
-          <div className="w-1/3">
-            <ul>
-              <li onClick={() => setActiveComponent("profile")}>Perfil</li>
-              <li onClick={() => setActiveComponent("account")}>Cuenta</li>
-            </ul>
-          </div>
+          <ul className="w-1/3">
+            <li
+              onClick={() => setActiveComponent("profile")}
+              className="cursor-pointer my-4"
+            >
+              <Profile className="inline" />{" "}
+              <span className="align-middle">Perfil</span>
+            </li>
+            <li
+              onClick={() => setActiveComponent("account")}
+              className="cursor-pointer my-4"
+            >
+              <Account className="inline" />{" "}
+              <span className="align-middle">Cuenta</span>
+            </li>
+          </ul>
 
           <SwitchComponents active={activeComponent}>
             <form className="w-1/3" name="profile">
