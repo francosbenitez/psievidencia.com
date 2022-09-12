e26dc06a274301597e83811eb1fb918767d48832
francobenitez <franco.benitez@hogarthww.com>
2022-09-12 10:34:45 -0300

    refactor: navbar

--- components/global/TheNavbar.tsx
+++ components/global/TheNavbar.tsx
@@ -14,6 +14,7 @@ import User from "@/public/icons/user.svg";
 import Power from "@/public/icons/power.svg";
 import Star from "@/public/icons/star.svg";
 import Pencil from "@/public/icons/pencil.svg";
+import Magnifier from "@/public/icons/magnifier.svg";
 
 const TheNavbar = (props: any) => {
   const { showLogin, showRegister, loginRef, registerRef }: any = props;
@@ -41,70 +42,77 @@ const TheNavbar = (props: any) => {
       {mounted && (
         <header className="w-full mx-auto py-5 navbar">
           <div className="container w-11/12 mx-auto">
-            <ul className="flex justify-end w-full">
-              {check != null ? (
-                <>
-                  <li className="p-2 flex">
-                    <div className="dropdown dropdown-left">
-                      <label tabIndex={0} className="cursor-pointer">
-                        <User />
-                      </label>
-                      <ul
-                        tabIndex={0}
-                        className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
-                      >
-                        <li>
-                          <Link href="/favorites">
-                            <a className="underline">
-                              <Star className="w-6 h-6" /> Mis favoritos
-                            </a>
-                          </Link>
-                        </li>
-                        <li>
-                          <Link href="/edit">
-                            <a className="underline">
-                              <Pencil className="w-6 h-6" /> Editar perfil
-                            </a>
-                          </Link>
-                        </li>
-                        <li>
-                          <button
-                            className="underline"
-                            onClick={() => handleLogout()}
-                          >
-                            <Power className="w-6 h-6" /> Cerrar sesión
-                          </button>
-                        </li>
-                      </ul>
-                    </div>
-                  </li>
-                </>
-              ) : (
-                <>
-                  <li className="p-2">
-                    <TheModal
-                      ref={loginRef}
-                      button={<LoginBtn />}
-                      title={"Ingresar"}
-                      content={<TheLogin showRegister={showRegister} />}
-                      modalCentered={true}
-                      modalMask={true}
-                      showRegister={showRegister}
-                    />
-                  </li>
-                  <li className="p-2">
-                    <TheModal
-                      ref={registerRef}
-                      button={<RegisterBtn />}
-                      title={"Registrarse"}
-                      content={<TheRegister showLogin={showLogin} />}
-                      modalCentered={true}
-                      modalMask={true}
-                      showLogin={showLogin}
-                    />
-                  </li>
-                </>
-              )}
+            <ul className="flex w-full">
+              <li className="flex my-auto">
+                <Link href="/">
+                  <Magnifier className="w-8 h-8 flex" />
+                </Link>
+              </li>
+              <div className="flex ml-auto">
+                {check != null ? (
+                  <>
+                    <li className="p-2 flex">
+                      <div className="dropdown dropdown-left">
+                        <label tabIndex={0} className="cursor-pointer">
+                          <User />
+                        </label>
+                        <ul
+                          tabIndex={0}
+                          className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+                        >
+                          <li>
+                            <Link href="/favorites">
+                              <a className="underline">
+                                <Star className="w-6 h-6" /> Mis favoritos
+                              </a>
+                            </Link>
+                          </li>
+                          <li>
+                            <Link href="/edit">
+                              <a className="underline">
+                                <Pencil className="w-6 h-6" /> Editar perfil
+                              </a>
+                            </Link>
+                          </li>
+                          <li>
+                            <button
+                              className="underline"
+                              onClick={() => handleLogout()}
+                            >
+                              <Power className="w-6 h-6" /> Cerrar sesión
+                            </button>
+                          </li>
+                        </ul>
+                      </div>
+                    </li>
+                  </>
+                ) : (
+                  <>
+                    <li className="p-2">
+                      <TheModal
+                        ref={loginRef}
+                        button={<LoginBtn />}
+                        title={"Ingresar"}
+                        content={<TheLogin showRegister={showRegister} />}
+                        modalCentered={true}
+                        modalMask={true}
+                        showRegister={showRegister}
+                      />
+                    </li>
+                    <li className="p-2">
+                      <TheModal
+                        ref={registerRef}
+                        button={<RegisterBtn />}
+                        title={"Registrarse"}
+                        content={<TheRegister showLogin={showLogin} />}
+                        modalCentered={true}
+                        modalMask={true}
+                        showLogin={showLogin}
+                      />
+                    </li>
+                  </>
+                )}
+              </div>
             </ul>
           </div>
         </header>
