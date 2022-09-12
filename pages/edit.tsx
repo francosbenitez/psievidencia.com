e7385fad6a6a4a546b1d94fa99c6101d224787c1
francobenitez <franco.benitez@hogarthww.com>
2022-09-12 11:41:34 -0300

    feat: add 'edit' view

---
+++ pages/edit.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Edit = () => {
+  return (
+    <div className="container min-h-screen w-11/12 mx-auto pt-20 pb-40">
+      <h2 className="text-3xl">Editá tus datos</h2>
+    </div>
+  );
+};
+
+export default Edit;
