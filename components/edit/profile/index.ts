2fef787784afaa54e7deb6437c1d730b4ad78806
francobenitez <franco.benitez@hogarthww.com>
2022-09-12 16:43:22 -0300

    feat: add & implement switchcomponents component

--- components/edit/profile/index.ts
+++ components/edit/profile/index.ts
@@ -1,3 +1,5 @@
 export { default as EditAdditionalData } from "./EditAdditionalData";
 export { default as EditCity } from "./EditCity";
 export { default as EditEducation } from "./EditEducation";
+export { default as EditGenderIdentity } from "./EditGenderIdentity";
+export { default as EditGenderPerspective } from "./EditGenderPerspective";
--- pages/edit.tsx
+++ pages/edit.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Head from "next/head";
-import * as EditComponents from "@/components/edit/profile";
+import * as EditProfileComponents from "@/components/edit/profile";
+import * as EditAccountComponents from "@/components/edit/account";
+import SwitchComponents from "@/components/edit/SwitchComponents";
 
 const Edit = () => {
-  const Components: Record<string, any> = EditComponents;
+  const ProfileComponents: Record<string, any> = EditProfileComponents;
+  const AccountComponents: Record<string, any> = EditAccountComponents;
+
+  const [activeComponent, setActiveComponent] = useState("profile");
+
   return (
     <>
       <Head>
@@ -11,12 +17,29 @@ const Edit = () => {
       </Head>
       <div className="container min-h-screen w-11/12 mx-auto pt-20 pb-40">
         <h2 className="text-3xl">Editá tus datos</h2>
-        <form className="">
-          {Object.keys(EditComponents).map((item) => {
-            const Item: any = Components[item];
-            return <Item key={item} />;
-          })}
-        </form>
+        <div className="flex">
+          <div className="w-1/3">
+            <ul>
+              <li onClick={() => setActiveComponent("profile")}>Perfil</li>
+              <li onClick={() => setActiveComponent("account")}>Cuenta</li>
+            </ul>
+          </div>
+
+          <SwitchComponents active={activeComponent}>
+            <form className="w-1/3" name="profile">
+              {Object.keys(EditProfileComponents).map((item) => {
+                const Item: any = ProfileComponents[item];
+                return <Item key={item} />;
+              })}
+            </form>
+            <form className="w-1/3" name="account">
+              {Object.keys(EditAccountComponents).map((item) => {
+                const Item: any = AccountComponents[item];
+                return <Item key={item} />;
+              })}
+            </form>
+          </SwitchComponents>
+        </div>
       </div>
     </>
   );
