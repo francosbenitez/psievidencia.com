e83fe60065048e0feebb11c0b4c072ccd4090f9f
francobenitez <franco.benitez@hogarthww.com>
2022-09-16 23:44:33 -0300

    feat: implement first api of password resetting

--- components/global/TheReset.tsx
+++ components/global/TheReset.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import UsersService from "@/services/UsersService";
 
 const TheReset = ({
   showModal,
@@ -7,24 +8,58 @@ const TheReset = ({
   showModal?: any;
   showLogin: any;
 }) => {
+  const [email, setEmail] = useState<string>("");
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    try {
+      const form = {
+        email: email,
+        redirect_url: "http://localhost:3000",
+      };
+
+      let formData = new FormData();
+      Object.entries(form).forEach(([key, value]) => {
+        formData.append(key, value);
+      });
+
+      await UsersService.resetPasswordStepOne(formData);
+      showModal();
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
   return (
-    <div>
-      <div>Restablecer mi contraseña</div>
-      <div>
-        Para recuperar tu contraseña, ingresá tu dirección de e-mail en el campo
-        inferior.
-      </div>
-      <input />
-      <button className="underline">Enviar</button>
+    <>
+      <form className="w-full" onSubmit={handleSubmit}>
+        <label>
+          Email
+          <div className="relative flex mb-3">
+            <input
+              type="text"
+              className="h-10 sm:h-full border border-primary w-full outline-0 rounded p-2"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          </div>
+        </label>
+        <button
+          type="submit"
+          className="bg-primary text-white py-2 px-4 rounded"
+        >
+          Recuperar
+        </button>
+      </form>
       <button
+        className="block mt-3"
         onClick={() => {
           showModal(), showLogin();
         }}
-        className="underline"
       >
-        Volver al login
+        ← Volver
       </button>
-    </div>
+    </>
   );
 };
 
