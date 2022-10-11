845d5f3955bad07821b58ee35c8b31fa0b830a79
francobenitez <franco.benitez@hogarthww.com>
2022-10-11 19:22:57 -0300

    feat: add head title to about, collaborate & contact views

--- pages/collaborate.tsx
+++ pages/collaborate.tsx
@@ -1,90 +1,96 @@
 import React from "react";
 import Link from "next/link";
+import Head from "next/head";
 
 const Collaborate = () => {
   return (
-    <div className="container min-h-screen w-11/12 mx-auto pt-20 pb-40">
-      <h2 className="text-3xl text-center underline">Colaborar</h2>
-      <div className="text-xl leading-normal mt-6">
-        <p className="mb-6">¡Hola! ¿Cómo estás?</p>
-        <p className="mb-6">
-          Primero que nada, queríamos agradecerte por tu interés. Siempre
-          estamos encantades de ayudar y recibir ayuda.
-        </p>
-        <p className="mb-6">
-          Para colaborar con Psievidencia, existen diferentes opciones:
-        </p>
-        <ol>
-          <li className="mb-6">
-            <span className="font-bold">Programación</span>
-            <p>
-              En esta área podés colaborar ya sea generando un{" "}
-              <a
-                href="https://github.com/francosbenitez/psievidencia.com"
-                target="_blank"
-                rel="noreferrer"
-                className="underline"
-              >
-                pull request
-              </a>{" "}
-              o{" "}
-              <Link href="/contact">
-                <a className="underline">contactándonos</a>
-              </Link>{" "}
-              para dar una mano más de cerca con aspectos de desarrollo.
-            </p>
-          </li>
-          <li className="mb-6">
-            <span className="font-bold">Diseño</span>
-            <p>
-              Como sabrás, este sitio aún no cuenta con las mejores prácticas en
-              términos de UX/UI, ni el mejor logo, etc. Si te gustaría aportar
-              desde este lado, ¡sos{" "}
-              <Link href="/contact">
-                <a className="underline">más que bienvenide</a>
-              </Link>
-              !
-            </p>
-          </li>
-          <li className="mb-6">
-            <span className="font-bold">Cafecito</span>
-            <p>
-              Contamos con un{" "}
-              <a
-                href="https://cafecito.app/psievidencia"
-                className="underline"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Cafecito
-              </a>
-              , principalmente para paliar todas las cuestiones relativas al
-              servidor, hosting y dominio del sitio. Además, el Cafecito
-              funciona como un medio para incentivar el laburo que venimos
-              haciendo entre todes. (En realidad, yo prefiero una pizza, pero un
-              Cafecito viene súper bien).
-            </p>
-          </li>
-          <li className="mb-6">
-            <span className="font-bold">Feedback</span>
-            <p>
-              Este sitio siempre va a estar en continua construcción, tratando
-              de paliar al máximo tanto las necesidades de los psicólogues, como
-              aquellas provenientes de les usuaries. Así que, por ello, ¡te
-              invitamos a{" "}
-              <Link href="/contact">
-                <a className="underline">escribirnos</a>
-              </Link>{" "}
-              con cualquier sugerencia que tengas!
-            </p>
-          </li>
-        </ol>
-        <p className="mb-6">
-          Dicho eso, sea que puedas o no colaborar, queremos agradecerte
-          igualmente tu visita.
-        </p>
+    <>
+      <Head>
+        <title>Colaborar | Psievidencia</title>
+      </Head>
+      <div className="container min-h-screen w-11/12 mx-auto pt-20 pb-40">
+        <h2 className="text-3xl text-center underline">Colaborar</h2>
+        <div className="text-xl leading-normal mt-6">
+          <p className="mb-6">¡Hola! ¿Cómo estás?</p>
+          <p className="mb-6">
+            Primero que nada, queríamos agradecerte por tu interés. Siempre
+            estamos encantades de ayudar y recibir ayuda.
+          </p>
+          <p className="mb-6">
+            Para colaborar con Psievidencia, existen diferentes opciones:
+          </p>
+          <ol>
+            <li className="mb-6">
+              <span className="font-bold">Programación</span>
+              <p>
+                En esta área podés colaborar ya sea generando un{" "}
+                <a
+                  href="https://github.com/francosbenitez/psievidencia.com"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="underline"
+                >
+                  pull request
+                </a>{" "}
+                o{" "}
+                <Link href="/contact">
+                  <a className="underline">contactándonos</a>
+                </Link>{" "}
+                para dar una mano más de cerca con aspectos de desarrollo.
+              </p>
+            </li>
+            <li className="mb-6">
+              <span className="font-bold">Diseño</span>
+              <p>
+                Como sabrás, este sitio aún no cuenta con las mejores prácticas
+                en términos de UX/UI, ni el mejor logo, etc. Si te gustaría
+                aportar desde este lado, ¡sos{" "}
+                <Link href="/contact">
+                  <a className="underline">más que bienvenide</a>
+                </Link>
+                !
+              </p>
+            </li>
+            <li className="mb-6">
+              <span className="font-bold">Cafecito</span>
+              <p>
+                Contamos con un{" "}
+                <a
+                  href="https://cafecito.app/psievidencia"
+                  className="underline"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Cafecito
+                </a>
+                , principalmente para paliar todas las cuestiones relativas al
+                servidor, hosting y dominio del sitio. Además, el Cafecito
+                funciona como un medio para incentivar el laburo que venimos
+                haciendo entre todes. (En realidad, yo prefiero una pizza, pero
+                un Cafecito viene súper bien).
+              </p>
+            </li>
+            <li className="mb-6">
+              <span className="font-bold">Feedback</span>
+              <p>
+                Este sitio siempre va a estar en continua construcción, tratando
+                de paliar al máximo tanto las necesidades de los psicólogues,
+                como aquellas provenientes de les usuaries. Así que, por ello,
+                ¡te invitamos a{" "}
+                <Link href="/contact">
+                  <a className="underline">escribirnos</a>
+                </Link>{" "}
+                con cualquier sugerencia que tengas!
+              </p>
+            </li>
+          </ol>
+          <p className="mb-6">
+            Dicho eso, sea que puedas o no colaborar, queremos agradecerte
+            igualmente tu visita.
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,78 +1,84 @@
 import React from "react";
+import Head from "next/head";
 
 const Contact = () => {
   return (
-    <div className="container min-h-screen w-11/12 mx-auto pt-20 pb-40">
-      <div className="flex flex-col justify-center sm:px-6 lg:px-8">
-        <h2 className="text-3xl text-center underline">Contacto</h2>
-        <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-lg">
-          <div className="bg-white py-8 px-4 border shadow sm:rounded-lg sm:px-10">
-            <form action="#">
-              <div className="mt-6 grid grid-cols-1 row-gap-6 col-gap-4 sm:grid-cols-6">
-                <div className="mb-5 sm:col-span-6">
-                  <label
-                    // for="name"
-                    className="block text-sm font-medium leading-5 text-primary"
-                  >
-                    Tu nombre
-                  </label>
-                  <div className="mt-1 rounded-md shadow-sm">
-                    <input
-                      name="name"
-                      type="text"
-                      id="name"
-                      className="appearance-none block w-full px-3 py-2 border border-primary rounded-md shadow-sm placeholder-primary focus:outline-none focus:bg-white focus:border-primary sm:text-sm "
-                      value=""
-                    />
+    <>
+      <Head>
+        <title>Contacto | Psievidencia</title>
+      </Head>
+      <div className="container min-h-screen w-11/12 mx-auto pt-20 pb-40">
+        <div className="flex flex-col justify-center sm:px-6 lg:px-8">
+          <h2 className="text-3xl text-center underline">Contacto</h2>
+          <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-lg">
+            <div className="bg-white py-8 px-4 border shadow sm:rounded-lg sm:px-10">
+              <form action="#">
+                <div className="mt-6 grid grid-cols-1 row-gap-6 col-gap-4 sm:grid-cols-6">
+                  <div className="mb-5 sm:col-span-6">
+                    <label
+                      // for="name"
+                      className="block text-sm font-medium leading-5 text-primary"
+                    >
+                      Tu nombre
+                    </label>
+                    <div className="mt-1 rounded-md shadow-sm">
+                      <input
+                        name="name"
+                        type="text"
+                        id="name"
+                        className="appearance-none block w-full px-3 py-2 border border-primary rounded-md shadow-sm placeholder-primary focus:outline-none focus:bg-white focus:border-primary sm:text-sm "
+                        value=""
+                      />
+                    </div>
+                  </div>
+                  <div className="mb-5 sm:col-span-6">
+                    <label
+                      // for="email"
+                      className="block text-sm font-medium leading-5 text-primary"
+                    >
+                      Tu email
+                    </label>
+                    <div className="mt-1 rounded-md shadow-sm">
+                      <input
+                        name="email"
+                        type="email"
+                        id="email"
+                        className="appearance-none block w-full px-3 py-2 border border-primary rounded-md shadow-sm placeholder-primary focus:outline-none focus:bg-white focus:border-primary sm:text-sm "
+                        value=""
+                      />
+                    </div>
+                  </div>
+                  <div className="mb-5 sm:col-span-6">
+                    <label
+                      // for="message"
+                      className="block text-sm font-medium leading-5 text-primary"
+                    >
+                      Tu mensaje
+                    </label>
+                    <div className="mt-1 rounded-md shadow-sm">
+                      <textarea
+                        name="message"
+                        // type="text"
+                        id="message"
+                        className="appearance-none block w-full px-3 py-2 border border-primary rounded-md shadow-sm placeholder-primary focus:outline-none focus:bg-white focus:border-primary sm:text-sm "
+                      ></textarea>
+                    </div>
                   </div>
                 </div>
-                <div className="mb-5 sm:col-span-6">
-                  <label
-                    // for="email"
-                    className="block text-sm font-medium leading-5 text-primary"
+                <span className="block rounded-md shadow-sm">
+                  <button
+                    type="submit"
+                    className="mt-5 w-full px-6 py-3 border border-transparent text-sm leading-4 uppercase font-medium rounded-md text-white bg-primary hover:bg-white hover:text-primary hover:border-primary focus:outline-none transition ease-in-out duration-150"
                   >
-                    Tu email
-                  </label>
-                  <div className="mt-1 rounded-md shadow-sm">
-                    <input
-                      name="email"
-                      type="email"
-                      id="email"
-                      className="appearance-none block w-full px-3 py-2 border border-primary rounded-md shadow-sm placeholder-primary focus:outline-none focus:bg-white focus:border-primary sm:text-sm "
-                      value=""
-                    />
-                  </div>
-                </div>
-                <div className="mb-5 sm:col-span-6">
-                  <label
-                    // for="message"
-                    className="block text-sm font-medium leading-5 text-primary"
-                  >
-                    Tu mensaje
-                  </label>
-                  <div className="mt-1 rounded-md shadow-sm">
-                    <textarea
-                      name="message"
-                      // type="text"
-                      id="message"
-                      className="appearance-none block w-full px-3 py-2 border border-primary rounded-md shadow-sm placeholder-primary focus:outline-none focus:bg-white focus:border-primary sm:text-sm "
-                    ></textarea>
-                  </div>
-                </div>
-              </div>
-              <span className="block rounded-md shadow-sm">
-                <button
-                  type="submit"
-                  className="mt-5 w-full px-6 py-3 border border-transparent text-sm leading-4 uppercase font-medium rounded-md text-white bg-primary hover:bg-white hover:text-primary hover:border-primary focus:outline-none transition ease-in-out duration-150"
-                >
-                  Enviar mensaje
-                </button>
-              </span>
-            </form>
+                    Enviar mensaje
+                  </button>
+                </span>
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
