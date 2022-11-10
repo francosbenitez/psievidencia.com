f4c39922ceb710df6478fecb6a12637452334861
Franco <francosbenitez@gmail.com>
2022-11-09 21:26:23 -0300

    refactor: change hardcoded api url

--- services/Api.ts
+++ services/Api.ts
@@ -6,7 +6,7 @@ import Router from "next/router";
 
 const API_URL =
   process.env.NEXT_PUBLIC_API_URL ||
-  "https://web-production-979a.up.railway.app/api/";
+  "https://web-develop-ecb4.up.railway.app/api/";
 
 export default () => {
   // const token: string | null = typeof window !== "undefined" ? getToken() : "";
