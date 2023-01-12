b10e812e6af7b0bf95c487778212358307c481b9
Franco <francosbenitez@gmail.com>
2023-01-12 18:51:19 -0300

    feat: setup redux

---
+++ store/index.ts
@@ -0,0 +1,25 @@
+import { createWrapper } from "next-redux-wrapper";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import reducers from "./reducers";
+
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
+const initialState = {
+  userLogin: { userInfo: userInfoFromStorage },
+};
+
+const middleware = [thunk];
+
+const store = createStore(
+  reducers,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+const wrapper = createWrapper(store);
+
+export default wrapper;
