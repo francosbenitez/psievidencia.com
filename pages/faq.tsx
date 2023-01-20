6b1a8098b19df8a31bd235be5cc2dc5d214fe798
Franco <francosbenitez@gmail.com>
2023-01-20 00:13:28 -0300

    feat: finish faq

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -4,22 +4,88 @@ import MuiAccordionSummary from "@mui/material/AccordionSummary";
 import MuiAccordionDetails from "@mui/material/AccordionDetails";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Head from "next/head";
+import Link from "next/link";
 
 const FAQ = () => {
   const faqs = [
     {
       title: "¿Cómo me registro en la web?",
-      content:
-        "Para registrarte en la web como profesional de psico, dirigite a <a class='underline' href='https://docs.google.com/forms/d/1KHCPdTENlypWSJGPOGsLB5uud7-Kxl91oEspAkJmyvU/viewform?edit_requested=true' target='_blank' rel=noopener noreferrer'>este Google Forms</a>. De momento, estamos usando este método, hasta que podamos integrar un formulario en la web (¡lo cual se viene pronto!).",
+      content: (
+        <>
+          Para registrarte en la web como profesional de psico, dirigite a{" "}
+          {
+            <Link
+              href="https://docs.google.com/forms/d/1KHCPdTENlypWSJGPOGsLB5uud7-Kxl91oEspAkJmyvU/viewform?edit_requested=true"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span className="underline cursor-pointer">
+                este Google Forms
+              </span>
+            </Link>
+          }
+          . De momento, estamos usando este método, hasta que podamos integrar
+          un formulario en la web (¡lo cual se viene pronto!).
+        </>
+      ),
     },
     {
       title: "¿Cómo colaboro con Psievidencia?",
-      content:
-        "¡Encantadísimos de sumar nuevos #Psievidentes! Dirigite a <a class='underline' href='/collaborate'>Colaborar</a> para saber más.",
+      content: (
+        <>
+          ¡Encantadísimos de sumar nuevos bancantes del proyecto! Dirigite a{" "}
+          {
+            <Link href="/collaborate">
+              <span className="underline cursor-pointer">Colaborar</span>
+            </Link>
+          }{" "}
+          para saber más.
+        </>
+      ),
     },
     {
       title: "¿Cómo puedo editar mi perfil?",
       content: "¡Todavía estamos trabajando en esa funcionalidad!",
+    },
+    {
+      title: "¿Existe un proceso de verificación de los perfiles?",
+      content: (
+        <>
+          De momento no, aún no está esa posibilidad. Pero si creés que alguien
+          posee una identidad falsa y/o alude a formaciones que no posee, podés
+          denunciarlo{" "}
+          {
+            <Link href="/contact">
+              <span className="underline cursor-pointer">contactándonos</span>
+            </Link>
+          }
+          .
+        </>
+      ),
+    },
+    {
+      title: "¿Es válido para otros países?",
+      content: (
+        <>
+          La mayoría de les psicoterapeutas atiende de manera virtual, ¡por lo
+          tanto sí! Es válido para todos los países.
+        </>
+      ),
+    },
+    {
+      title: "¿Se cobra por pertenecer a Psievidencia?",
+      content: (
+        <>
+          No, Psievidencia es completamente gratuito. Si te interesaría aportar
+          tu granito de arena al proyecto, dirígete a{" "}
+          {
+            <Link href="/collaborate">
+              <span className="underline cursor-pointer">Colaborar</span>
+            </Link>
+          }
+          .
+        </>
+      ),
     },
   ];
 
@@ -40,23 +106,21 @@ const FAQ = () => {
           </p>
         </div>
         <div className="mt-6 mx-auto max-w-2xl">
-          {faqs.map((faq) => (
-            <Accordion key={faq.title} sx={{ marginBottom: "1rem" }}>
-              <MuiAccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
-              >
-                <p className="text-lg">{faq.title}</p>
-              </MuiAccordionSummary>
-              <MuiAccordionDetails>
-                <p
-                  className="text-lg"
-                  dangerouslySetInnerHTML={{ __html: faq.content }}
-                />
-              </MuiAccordionDetails>
-            </Accordion>
-          ))}
+          {faqs.map((faq) => {
+            console.log("faq.content", faq.content);
+            return (
+              <Accordion key={faq.title} sx={{ marginBottom: "1rem" }}>
+                <MuiAccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <p className="text-lg">{faq.title}</p>
+                </MuiAccordionSummary>
+                <MuiAccordionDetails>{faq.content}</MuiAccordionDetails>
+              </Accordion>
+            );
+          })}
         </div>
       </div>
     </>
